3a989c8e97fc233ffcf7374e036073497acaf70d
ZUENS2020 <shengyuanzhang-zuens2020@outlook.com>
2025-12-12 17:57:35 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: ['music.zuens2020.work'],
       },
       plugins: [react()],
       define: {
